7e1cb54c45d647c1bb16553a5f27b4ce72c50f38
Mike Bédard <mike01bedard@gmail.com>
2022-07-22 12:04:05 -0400

    update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const { BaseURL } = require("./src/api/util.js")
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '',
   devServer: {
     proxy: BaseURL
   }
